b7530ea5eb2589c97708a77eddf730ea7f7f41be
Malu <marialneguinha@gmail.com>
2026-06-26 20:56:29 -0300

    mudei o nome da conexão com o model, porque estava errado

--- controllers/EmprestimoController.php
+++ controllers/EmprestimoController.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../models/EmprestimoModel.php';
+require_once __DIR__ . '/../models/Emprestimo.php';
 
 
 class EmprestimoController
