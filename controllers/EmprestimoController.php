8679fc60ccd5020881804511756d92b21d55148d
Malu <marialneguinha@gmail.com>
2026-06-26 20:53:17 -0300

    adicionei o controller de emprestimo

---
+++ controllers/EmprestimoController.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../models/EmprestimoModel.php';
+
+
+class EmprestimoController
+{
+    public static function registrarEmprestimo(int $exemplarId, string $alunoMatricula): int
+    {
+        $pdo = getConnection();
+        return EmprestimoModel::insert($pdo, $exemplarId, $alunoMatricula);
+    }
+
+    public static function listar(): array
+    {
+        $pdo = getConnection();
+        return EmprestimoModel::getAll($pdo);
+    }
+
+    public static function listarEmAberto(): array
+    {
+        $pdo = getConnection();
+        return EmprestimoModel::getEmAberto($pdo);
+    }
+
+    public static function buscarPorId(int $id): array|false
+    {
+        $pdo = getConnection();
+        return EmprestimoModel::getById($pdo, $id);
+    }
+
+    public static function devolver(int $emprestimoId): bool
+    {
+        $pdo = getConnection();
+        return EmprestimoModel::registrarDevolucao($pdo, $emprestimoId);
+    }
+
+    public static function remover(int $id): bool
+    {
+        $pdo = getConnection();
+        return EmprestimoModel::delete($pdo, $id);
+    }
+}
