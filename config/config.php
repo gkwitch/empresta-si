d7b971f93f8d1199b21cc968690b7f69cd6aa627
gkwitch <gleicykethelin@ufrrj.br>
2026-06-30 21:02:45 -0300

    mudando url

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Configurações gerais
-define('BASE_URL', 'http://localhost/empresta_si/public/');
+define('BASE_URL', 'http://localhost/empresta-si/public/');
 
 // Configurações do Banco de Dados
 define('DB_HOST', '127.0.0.1');
